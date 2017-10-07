f6003e815fe95c941922f35b69a3c2aa7a65fb8f
IAmNotHanni <cmdfreak@web.de>
2017-10-07 14:34:56 +0200

    Improved home page.

--- src/Home.vue
+++ src/Home.vue
@@ -1,35 +1,41 @@
 <template>
   <div class="container">
-    <img id="inexor_logo" class="img-fluid" src="src/assets/logo_rendered/inexor_logo_600.png">
-    <h1 class="cover-heading" id="inexor_headline">An open-source first person shooter forked from Cube 2</h1>
+    <img id="inexor_logo" class="img-fluid" src="src/assets/logo_rendered/inexor_logo_400.png">
+    <h1 class="cover-heading" id="inexor_headline">Fast classic gameplay crossed with modern technology.</h1>
+
     <p class="lead" id="inexor_description">
        It adds a lot of functionality and tries to stay open for improvements and suggestions.
        The task is to be more flexible and to create an environment where development is easy and fast and creativity can prosper.
     </p>
+
     <p class="lead">
       <a href="https://github.com/inexorgame/code/wiki/Install-Inexor-in-five-minutes" class="btn btn-lg btn-secondary inexor_button">DOWNLOAD</a>
     </p>
+
   </div>
 </template>
 
 <style>
 
 .inexor_button {
-   margin-top:100px;
-   border-radius:0px;
+  margin-top:100px;
+  border-radius:0px;
 }
 
 #inexor_logo {
-   margin-bottom:30px;
+  margin-top:50px;
+  margin-bottom:50px;
 }
 
 #inexor_headline {
-   font-size:37.6px;
-   margin-bottom:30px;
+  font-size:37.6px;
+  margin-bottom:30px;
 }
 #inexor_description {
-   font-family:arial;
-   font-size:24px;
+  padding:10px;
+  font-family:arial;
+  font-size:24px;
+  background-color:rgba(0,0,0,0.5);
 }
 
 </style>
