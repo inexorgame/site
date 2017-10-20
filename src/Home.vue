ad3cf9eb402005e5372dad55bfcf108f083121ac
Saeed A <alalwan@gmail.com>
2017-10-20 11:21:31 -0400

    support for dynamic download links

--- src/Home.vue
+++ src/Home.vue
@@ -29,36 +29,56 @@
     <div class="container">
       <div class="row justify-content-center">
         <div class="col col-md-8">
-          <a href="/download" id="inexor_button" class="btn btn-lg btn-secondary">Download</a>
+          <a :href="getLink.link" id="inexor_button" class="btn btn-lg btn-secondary">Download <i :class="getLink.icon" aria-hidden="true"></i></a>
+        </div>
+         <div class="col col-md-8 download-link">
+            <router-link class="nav-link" to="/download">Other Downloads</router-link>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  props: ["linux_link", "windows_link"],
+  computed: {
+    getLink() {
+      const link = navigator.platform.toLocaleUpperCase() == "WIN32" ? {link: this.windows_link, icon: "fa fa-windows"}: {link: this.linux_link, icon: "fa fa-linux"};
+      return link;
+    }
+  }
+};
+</script>
 
+<style scoped>
 #inexor_headline {
   margin-top: 30px;
 }
 #blackbox {
   margin-top: 30px;
-  background-color:rgba(0,0,0,0.7);
-  border-top:rgba(255,255,255,0.5) 1px solid;
-  border-bottom:rgba(255,255,255,0.5) 1px solid;
+  background-color: rgba(0, 0, 0, 0.7);
+  border-top: rgba(255, 255, 255, 0.5) 1px solid;
+  border-bottom: rgba(255, 255, 255, 0.5) 1px solid;
 }
 #blackbox ul {
-  padding:10px 0px 10px 0px;
-  font-family:arial;
-  font-size:24px;
-  list-style:none;
-  text-align:left;
+  padding: 10px 0px 10px 0px;
+  font-family: arial;
+  font-size: 24px;
+  list-style: none;
+  text-align: left;
 }
 
 @keyframes inexor_blink {
-  0%   {box-shadow:0px 0px 10px 1px white;}
-  50%  {box-shadow:none;}
-  100% {box-shadow:0px 0px 10px 1px white;}
+  0% {
+    box-shadow: 0px 0px 10px 1px white;
+  }
+  50% {
+    box-shadow: none;
+  }
+  100% {
+    box-shadow: 0px 0px 10px 1px white;
+  }
 }
 
 #inexor_button {
@@ -68,15 +88,14 @@
   font-size: 24px;
   text-align: center;
   text-transform: uppercase;
-  background-color:white;
-  color:black;
-  border:0px;
-  border-radius:0px;
+  background-color: white;
+  color: black;
+  border: 0px;
+  border-radius: 0px;
   animation-name: inexor_blink;
-  animation-duration:3s;
+  animation-duration: 3s;
   animation-timing-function: ease;
   animation-iteration-count: infinite;
   animation-direction: reverse;
 }
-
 </style>
