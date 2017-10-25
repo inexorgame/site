9b60653c4b9edd1074994777f3bae93c08fe0eab
Lennard Berger <berger@mecodia.de>
2017-10-25 18:13:32 +0200

    [news] Add timeline template

--- src/News.vue
+++ src/News.vue
@@ -1,12 +1,24 @@
 <template>
-    <div>
-        <ul v-for="item in items">
-            <li>
-                <h4>{{item.title}}</h4>
-                {{item.description}}
-                <a :href="item.url">Written on {{item.created}}</a>
-            </li>
-        </ul>
+    <div class="inner cover">
+        <div class="container">
+            <div class="page-header">
+                <h1 id="timeline">News</h1>
+            </div>
+            <ul class="timeline" v-for="(item, index) in items">
+                <li v-bind:class="{ 'timeline-inverted': (index % 2 == 0) }">
+                    <div class="timeline-badge"><i :class="chooseBadge()" aria-hidden="true"></i></div>
+                    <div class="timeline-panel">
+                        <div class="timeline-heading">
+                            <a :href="item.url"><h4 class="timeline-title">{{item.title}}</h4></a>
+                            <p><small class="text-muted"><i class="fa fa-clock-o" aria-hidden="true"></i> {{item.created}} on {{item.feed.name}}</small></p>
+                        </div>
+                        <div class="timeline-body">
+                            {{ item.description }}
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -19,8 +31,165 @@ export default {
             items: []
         }
     },
+    methods: {
+      chooseBadge() {
+          let badges = ['newspaper-o', 'fire', 'font-awesome', 'comment']
+          return `fa fa-${badges[Math.floor(Math.random()*badges.length)]}`
+      }
+    },
     apollo: {
-        items: gql`{items { title, created, description, url }}`
+        items: gql`{ items { title, description, created, url, feed { name } } }`
     }
 }
 </script>
+
+<style>
+    .timeline {
+        list-style: none;
+        padding: 20px 0 20px;
+        position: relative;
+    }
+
+    .timeline:before {
+        top: 0;
+        bottom: 0;
+        position: absolute;
+        content: " ";
+        width: 3px;
+        background-color: #eeeeee;
+        left: 50%;
+        margin-left: -1.5px;
+    }
+
+    .timeline > li {
+        margin-bottom: 20px;
+        position: relative;
+    }
+
+    .timeline > li:before,
+    .timeline > li:after {
+        content: " ";
+        display: table;
+    }
+
+    .timeline > li:after {
+        clear: both;
+    }
+
+    .timeline > li:before,
+    .timeline > li:after {
+        content: " ";
+        display: table;
+    }
+
+    .timeline > li:after {
+        clear: both;
+    }
+
+    .timeline > li > .timeline-panel {
+        width: 46%;
+        float: left;
+        border: 1px solid #d4d4d4;
+        border-radius: 2px;
+        padding: 20px;
+        position: relative;
+        -webkit-box-shadow: 0 1px 6px rgba(0, 0, 0, 0.175);
+        box-shadow: 0 1px 6px rgba(0, 0, 0, 0.175);
+        background: rgba(0,0,0,0.7);
+    }
+
+    .timeline > li > .timeline-panel:before {
+        position: absolute;
+        top: 26px;
+        right: -15px;
+        display: inline-block;
+        border-top: 15px solid transparent;
+        border-left: 15px solid #ccc;
+        border-right: 0 solid #ccc;
+        border-bottom: 15px solid transparent;
+        content: " ";
+    }
+
+    .timeline > li > .timeline-panel:after {
+        position: absolute;
+        top: 27px;
+        right: -14px;
+        display: inline-block;
+        border-top: 14px solid transparent;
+        border-left: 14px solid #fff;
+        border-right: 0 solid #fff;
+        border-bottom: 14px solid transparent;
+        content: " ";
+    }
+
+    .timeline > li > .timeline-badge {
+        color: #fff;
+        width: 50px;
+        height: 50px;
+        line-height: 50px;
+        font-size: 1.4em;
+        text-align: center;
+        position: absolute;
+        top: 16px;
+        left: 50%;
+        margin-left: -25px;
+        background-color: #999999;
+        z-index: 100;
+        border-top-right-radius: 50%;
+        border-top-left-radius: 50%;
+        border-bottom-right-radius: 50%;
+        border-bottom-left-radius: 50%;
+    }
+
+    .timeline > li.timeline-inverted > .timeline-panel {
+        float: right;
+    }
+
+    .timeline > li.timeline-inverted > .timeline-panel:before {
+        border-left-width: 0;
+        border-right-width: 15px;
+        left: -15px;
+        right: auto;
+    }
+
+    .timeline > li.timeline-inverted > .timeline-panel:after {
+        border-left-width: 0;
+        border-right-width: 14px;
+        left: -14px;
+        right: auto;
+    }
+
+    .timeline-badge.primary {
+        background-color: #2e6da4 !important;
+    }
+
+    .timeline-badge.success {
+        background-color: #3f903f !important;
+    }
+
+    .timeline-badge.warning {
+        background-color: #f0ad4e !important;
+    }
+
+    .timeline-badge.danger {
+        background-color: #d9534f !important;
+    }
+
+    .timeline-badge.info {
+        background-color: #5bc0de !important;
+    }
+
+    .timeline-title {
+        margin-top: 0;
+        color: inherit;
+    }
+
+    .timeline-body > p,
+    .timeline-body > ul {
+        margin-bottom: 0;
+    }
+
+    .timeline-body > p + p {
+        margin-top: 5px;
+    }
+</style>
