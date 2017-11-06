2ac1ee01e5f8e1104b649dae1945a1d2183b8a00
Lennard Berger <berger@mecodia.de>
2017-11-06 23:56:46 +0100

    [links] Update download links to current early installer. Also snap should be updated.

--- src/downloadLinksMixin.js
+++ src/downloadLinksMixin.js
@@ -3,9 +3,9 @@ export default {
         return {
             downloadLinks: {
                 linux:
-                    "https://github.com/inexorgame/inexor-core/releases/",
+                    "https://github.com/inexorgame/snap-inexor-flex",
                 windows:
-                    "https://github.com/inexorgame/inexor-core/releases/",
+                    "https://github.com/inexorgame/windows-installer/releases/tag/0.1",
                 osx:
                     "https://github.com/inexorgame/inexor-core/issues/385"
             },
@@ -14,11 +14,13 @@ export default {
     },
     created() {
         this.loading = true;
+        /*
+        // This should be updated to individual releases of snap-inexor-flex and windows-installer
         this.$http.get(`https://api.github.com/repos/inexorgame/inexor-core/releases`).then((response) => {
             const { tag_name } = response.body[0];
             this.downloadLinks.windows = `https://github.com/inexorgame/inexor-core/releases/download/${tag_name}/Inexor-${tag_name}-win64.zip`;
             this.downloadLinks.linux = `https://github.com/inexorgame/inexor-core/releases/download/${tag_name}/Inexor-${tag_name}-Linux.zip`;
             this.loading = false;
-        })
+        })*/
     }
 }
