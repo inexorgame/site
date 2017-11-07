530ca69d0b73084ef4e525b2aa5f754ab4632f92
Lennard Berger <berger@mecodia.de>
2017-11-07 20:17:58 +0100

    [links] Update links to the actual installers

--- src/downloadLinksMixin.js
+++ src/downloadLinksMixin.js
@@ -3,9 +3,9 @@ export default {
         return {
             downloadLinks: {
                 linux:
-                    "https://github.com/inexorgame/snap-inexor-flex",
+                    "https://github.com/inexorgame/snap-inexor-flex/releass",
                 windows:
-                    "https://github.com/inexorgame/windows-installer/releases/tag/0.1",
+                    "https://github.com/inexorgame/windows-installer/releases/",
                 osx:
                     "https://github.com/inexorgame/inexor-core/issues/385"
             },
@@ -14,13 +14,25 @@ export default {
     },
     created() {
         this.loading = true;
-        /*
-        // This should be updated to individual releases of snap-inexor-flex and windows-installer
-        this.$http.get(`https://api.github.com/repos/inexorgame/inexor-core/releases`).then((response) => {
+
+        let windowsDownload = this.$http.get(`https://api.github.com/repos/inexorgame/windows-installer/releases`).then((response) => {
             const { tag_name } = response.body[0];
-            this.downloadLinks.windows = `https://github.com/inexorgame/inexor-core/releases/download/${tag_name}/Inexor-${tag_name}-win64.zip`;
-            this.downloadLinks.linux = `https://github.com/inexorgame/inexor-core/releases/download/${tag_name}/Inexor-${tag_name}-Linux.zip`;
+            this.downloadLinks.windows = `https://github.com/inexorgame/windows-installer/releases/download/${tag_name}/Inexor_Setup.exe`;
+            return new Promise((resolve) => resolve())
+        }).catch(() => {
+            return new Promise((resolve, reject) => reject())
+        })
+
+        let linuxDownload = this.$http.get('https://api.github.com/repos/inexorgame/snap-inexor-flex/releases').then((response) => {
+            const { tag_name } = response.body[0];
+            this.downloadLinks.linux = `https://github.com/inexorgame/snap-inexor-flex/releases/download/${tag_name}/inexor-flex_${tag_name}_amd64.snap`
+            return new Promise((resolve) => resolve())
+        }).catch(() => {
+            return new Promise((resolve, reject) => reject())
+        })
+
+        Promise.all(windowsDownload, linuxDownload).then(() => {
             this.loading = false;
-        })*/
+        })
     }
 }
