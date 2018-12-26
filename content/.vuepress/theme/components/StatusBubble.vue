585635903cfa91ba8bb21970534af12bbf202447
Martin Muzatko <martin@happy-css.com>
2018-12-26 01:15:57 +0100

    moved statusbubble to its own component

---
+++ content/.vuepress/theme/components/StatusBubble.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="status-bubble text-sm" :class="getStatusClass()">{{type}}</div>
+</template>
+
+<script>
+
+import { STATUS_TYPE_COLORS, STATUS_TYPES } from '../constants'
+
+export default {
+    props: {
+        inverted: {
+            type: Boolean,
+            default: false,
+        },
+        type: String,
+    },
+    methods: {
+        getStatusClass() {
+            let color = (STATUS_TYPE_COLORS[this.type] || 'blue')
+            if (this.inverted) return ['bg-white', `text-${color}`, `border-${color}` ]
+            return ['bg-' + color]
+        }
+    }
+}
+</script>
---
+++ content/.vuepress/theme/constants.js
@@ -0,0 +1,8 @@
+// use colors as defined here: https://tailwindcss.com/docs/colors/
+export const STATUS_TYPE_COLORS = {
+    idea: 'blue-dark',
+    concept: 'yellow-dark',
+    specification: 'orange-dark',
+    implemented: 'green-dark',
+}
+export const STATUS_TYPES = Object.keys(STATUS_TYPE_COLORS)
